b8af1fffbd876b9049afd051c955d062cd45deb1
MalinovskiyAleksandr <103271789+AleksandrMalinovskiy@users.noreply.github.com>
2022-05-12 12:20:06 +0300

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -43,13 +43,52 @@ if
 	[ "$r" = yes ];
 then
 
-# Установка prometheus
+cd /home/
 
-# Создаём папки и копируем файлы
+# Установка prometheus
+read -p "Создать папку prometheus?" r
+
+if
+	[ "$r" = y ];
+then
 mkdir {/etc/,/var/lib/}prometheus
+elif
+	[ "$r" = n ];
+then
+	echo "ERROR"
+	exit
+fi
+
+read -p "Копируем файлы?" r
+
+if
+	[ "$r" = y ];
+then
 cp -vi prometheus-*.linux-amd64/prom{etheus,tool} /usr/local/bin
+
+elif
+	[ "$r" = n ];
+then
+	echo "ERROR"
+	exit
+fi
+
 cp -rvi prometheus-*.linux-amd64/{console{_libraries,s},prometheus.yml} /etc/prometheus/
+
+read -p "Изменяем владельца?" r
+
+if
+	[ "$r" = y ];
+then
 chown -Rv prometheus: /usr/local/bin/prom{etheus,tool} /etc/prometheus/ /var/lib/prometheus/
+
+elif
+	[ "$r" = n ];
+then
+	echo "ERROR"
+	exit
+fi
+
 
 # Настраиваем сервис
 
@@ -67,6 +106,7 @@ systemctl start prometheus
 systemctl status prometheus
 systemctl enable prometheus
 
+
 elif
 	[ "$r" = no ];
 then
@@ -76,7 +116,6 @@ fi
 
 echo "Node_exporter установлен" 
 echo "Prometheus установлен"
-
 
 read -p "Установливаем grafana?(yes/no)" r
 
