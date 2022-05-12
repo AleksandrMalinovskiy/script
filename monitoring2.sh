769f639c793edfa5cf96ff52cf2601f23e5244a1
MalinovskiyAleksandr <103271789+AleksandrMalinovskiy@users.noreply.github.com>
2022-05-12 12:58:10 +0300

    Update monitoring2.sh

--- monitoring2.sh
+++ monitoring2.sh
@@ -1,49 +1,8 @@
 #!/bin/bash
-
-# Установка prometheus
-read -p "Создать папку prometheus?" r
-
-if
-	[ "$r" = y ];
-then
 mkdir {/etc/,/var/lib/}prometheus
-elif
-	[ "$r" = n ];
-then
-	echo "ERROR"
-	exit
-fi
-
-read -p "Копируем файлы?" r
-
-if
-	[ "$r" = y ];
-then
 cp -vi prometheus-*.linux-amd64/prom{etheus,tool} /usr/local/bin
-
-elif
-	[ "$r" = n ];
-then
-	echo "ERROR"
-	exit
-fi
-
 cp -rvi prometheus-*.linux-amd64/{console{_libraries,s},prometheus.yml} /etc/prometheus/
-
-read -p "Изменяем владельца?" r
-
-if
-	[ "$r" = y ];
-then
 chown -Rv prometheus: /usr/local/bin/prom{etheus,tool} /etc/prometheus/ /var/lib/prometheus/
-
-elif
-	[ "$r" = n ];
-then
-	echo "ERROR"
-	exit
-fi
-
 
 # Настраиваем сервис
 
